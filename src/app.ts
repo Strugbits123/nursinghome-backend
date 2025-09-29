899386edf3cd095f7b74de591b0645cd9cc231cc
Ammaz Rafi <ammaz@strugbitsglobal.com>
2025-09-29 20:04:27 +0500

    add

--- src/app.ts
+++ src/app.ts
@@ -75,7 +75,10 @@ const connectDB = async () => {
 
     // Start Express server
     const PORT: number = Number(process.env.PORT) || 5000;
-    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+    app.listen(PORT, '0.0.0.0', () =>
+      console.log(`🚀 Server running on port ${PORT}`)
+    );    
+    // app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
   } catch (err) {
     console.error('❌ MongoDB connection error:', err);
     process.exit(1);
